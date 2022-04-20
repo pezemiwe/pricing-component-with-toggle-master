82931a1a5425f815893540d440ea23b14fa45812
pezemiwe <pezemiwe@gmail.com>
2022-04-20 19:54:40 +0100

    made the prices change with the toggle switch

--- script.js
+++ script.js
@@ -7,7 +7,19 @@ const master = document.getElementById("master");
 toggle.addEventListener("click", () => {
   button.classList.toggle("pricing__switch--active");
   toggle.classList.toggle("pricing__toggle--active");
-  basic.innerText = "$199.99" ? "$19.99" : "$199.99";
-  pro.innerText = "$249.99" ? "$24.99" : "$249.99";
-  master.innerText = "$399.99" ? "$39.99" : "$399.99";
+  if (basic.innerText === "$199.99") {
+    basic.innerText = "$19.99";
+  } else {
+    basic.innerText = "$199.99";
+  }
+  if (pro.innerText === "$249.99") {
+    pro.innerText = "$24.99";
+  } else {
+    pro.innerText = "$249.99";
+  }
+  if (master.innerText === "$399.99") {
+    master.innerText = "$39.99";
+  } else {
+    master.innerText = "$399.99";
+  }
 });
